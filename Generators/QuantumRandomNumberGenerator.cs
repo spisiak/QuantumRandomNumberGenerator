bf70fa67ea9a7d923437f2fb29a875aea8b49d99
spisiak <spisiak.m@outlook.com>
2018-01-13 17:00:55 +0100

    Initial commit

---
+++ Generators/QuantumRandomNumberGenerator.cs
@@ -0,0 +1,57 @@
+﻿using QuantumCSharp;
+using QuantumCSharp.HistoryResult;
+using QuantumCSharp.Ibm;
+using System.Linq;
+
+namespace QuantumRandomNumbers
+{
+    public class QuantumRandomNumberGenerator
+    {
+        public QuantumRandomNumber GenerateNumber(QuantumProgram program, int QubitNumber)
+        {
+            program.Clear();
+            program.Options.Shots = 2048;
+            Qubit[] qubits = new Qubit[QubitNumber];
+            for(int i =0;i < QubitNumber;i++)
+            {
+                qubits[i] = new Qubit(program, i);
+                qubits[i].H();
+            }
+            var quantum_number =  new QuantumRandomNumber() { IbmResult = program.Execute() };
+            if (quantum_number.IbmResult.State == IbmQXSendState.Ok)
+            {
+                var tmp = program.GetQubitResultStates(quantum_number.IbmResult);
+                tmp = (from t in tmp orderby t.Index ascending select t).ToList();
+                short[] quantum_values = new short[QubitNumber];
+                for(int i = 0;i < QubitNumber;i++)
+                {
+                    quantum_values[i] = (short)(tmp[i].One % 100);
+                }
+                quantum_number.SetQuantumNumber(quantum_values);
+            }
+            return null;
+        }
+
+        public QuantumRandomNumber GenerateNumber(HistoryResult program, string HistoryResultId)
+        {
+            var items = program.GetHistoryResults().Result;
+            if (items.Count == 0)
+                return null;
+            var result = program.GetQubitResultStatesById(items, HistoryResultId);
+            if (result != null)
+            {
+                var quantum_number = new QuantumRandomNumber();
+                result = (from t in result orderby t.Index ascending select t).ToList();
+                short[] quantum_values = new short[result.Count];
+                for (int i = 0; i < result.Count; i++)
+                {
+                    quantum_values[i] = (short)(result[i].One % 100);
+                }
+                quantum_number.SetQuantumNumber(quantum_values);
+                return quantum_number;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Types/QRNG_Types.cs
@@ -0,0 +1,49 @@
+﻿using QuantumCSharp.Ibm;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace QuantumRandomNumbers
+{
+    public class QuantumRandomNumber
+    {
+        private short[] QuantumResults { get; set; }
+        public IbmQX_SendResult IbmResult { get; set; }
+
+        public void SetQuantumNumber(short[] Number)
+        {
+            this.QuantumResults = Number;
+        }
+
+        public Int64 ToInt64()
+        {
+            Int64 result = 0;
+            for(int i = 0;i < QuantumResults.Length && i < 9;i++)
+            {
+                result += QuantumResults[i] * (Int64)Math.Pow(100, i);
+            }
+            return result;
+        }
+
+        public BigInteger ToBigInteger()
+        {
+            byte[] tmp_array = new byte[QuantumResults.Length];
+            for (int i = 0; i < QuantumResults.Length; i++)
+            {
+                tmp_array[i] = (byte)QuantumResults[i];
+            }
+            return new BigInteger(tmp_array);
+        }
+
+        public override string ToString()
+        {
+            string new_format = "";
+            for (int i = 0; i < QuantumResults.Length; i++)
+            {
+                new_format += QuantumResults[i] + (i < (QuantumResults.Length-1) ? " " : "");
+            }
+            return new_format;
+        }
+    }
+}
